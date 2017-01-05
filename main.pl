242ffb22bc25ffbe27e7b91248f16c7d04e922ea
Den Patin <denpatin@gmail.com>
2017-01-05 03:11:49 +0300

    Done: Vowel harmony based inflection system draft

--- main.pl
+++ main.pl
@@ -12,10 +12,50 @@ cons(й). cons(к). cons(л). cons(м). cons(н). cons(ң).
 cons(п). cons(р). cons(с). cons(т). cons(ф). cons(х).
 cons(ц). cons(ч). cons(ш). cons(щ). cons(ь). cons(ъ).
 
+% Kyrgyz vowel harmony rules
+harmony(и, е). harmony(и, и). harmony(ү, ө). harmony(ү, ү).
+harmony(е, е). harmony(е, и). harmony(ө, ө). harmony(ө, ү).
+harmony(а, а). harmony(а, ы). harmony(о, о). harmony(о, у).
+harmony(ы, а). harmony(ы, ы). harmony(у, а). harmony(у, у).
+
 % Tracking down all vowels in a word via vowels/2: vowels(?Word, -Vowels)
 vowels([], []).
-vowels([V|T], [V|X]) :- vowel(V), vowels(T, X), !.
-vowels([C|T], X) :- cons(C), vowels(T, X).
+vowels([V|T], [V|X]) :-
+    vowel(V),
+    vowels(T, X),
+    !.
+vowels([C|T], X) :-
+    cons(C),
+    vowels(T, X).
 
-% Finding the last vowel in word via last_vowel/2: last_vowel(?Vowels, ?Vowel)
-last_vowel(L, X) :- vowels(L, V), last(V, X), !.
+% Finding the last vowel in [word] via last_vowel/2: last_vowel(?Vowels, ?Vowel)
+last_vowel(S, X) :-
+    last(S, S1),
+    string_chars(S1, L),
+    vowels(L, V),
+    last(V, X).
+
+% Finding the first [word] in list of variants via first/2: first(?List, ?Word)
+first(L, X) :-
+    last(L, E),
+    select(E, L, X),
+    !.
+
+inflection(S1, S3) :-
+    noun(S1, S2),
+    suffix(S2, S3),
+    first(S1, NV),
+    last_vowel(NV, V1),
+    last_vowel(S2, V2),
+    harmony(V1, V2).
+
+% Tests
+suffix --> [чу].
+suffix --> [чи].
+suffix --> [чы].
+suffix --> [чү].
+noun --> [кой]. % кой-чу
+noun --> [мерген]. % мерген-чи
+noun --> [балык]. % балык-чы
+noun --> [сүрөт]. % сүрөт-чү
+% inflection(X, []).
